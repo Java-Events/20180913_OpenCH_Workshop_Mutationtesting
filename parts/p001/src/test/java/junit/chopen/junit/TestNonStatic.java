041bb6673b668431e55e00d98e3935bebe70bf32
dve <dve@vergien.net>
2018-09-13 08:48:43 +0200

    junit basic examples

---
+++ parts/p001/src/test/java/junit/chopen/junit/TestNonStatic.java
@@ -0,0 +1,47 @@
+package junit.chopen.junit;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+@TestInstance(Lifecycle.PER_CLASS)
+class TestNonStatic {
+
+	@BeforeAll
+	void beforeAll() {
+		System.out.println("Before all tests");
+	}
+
+	@AfterAll
+	void afterAll() {
+		System.out.println("After all tests");
+	}
+
+	@BeforeEach
+	void beforeEach() {
+		System.out.println("Before each test");
+	}
+
+	@AfterEach
+	void afterEach() {
+		System.out.println("After each test");
+	}
+
+	@Test
+	@DisplayName("The first test")
+	void test001() {
+		System.out.println("Test 001");
+	}
+
+	@Test
+	@DisplayName("The second test")
+	void test002() {
+		System.out.println("Test 002");
+	}
+
+}
